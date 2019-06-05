a8e28e3de0cf956f34171b4d248adf252ba26b53
kyleschaeffer <kyle@spirito.us>
2019-06-05 15:56:15 -0400

    Implement onSort lifecycle event

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,15 @@
 import { Sortable } from './sortable'
 
 // Sortables
-const sortables = new Sortable()
+const sortables = new Sortable({
+  onSort: state => {
+    // Get sortable data
+    const items = []
+    for (let li of state.sortableList.children) {
+      items.push(li.textContent.trim())
+    }
+
+    // Update JSON
+    document.getElementById('json').value = JSON.stringify(items, null, 2)
+  },
+})
